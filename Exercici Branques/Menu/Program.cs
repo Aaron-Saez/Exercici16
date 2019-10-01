2928240f2912e0e6e2e43fc0bc4067c4e1404d90
Aaron Saez <41679803p@institutmontilivi.cat>
2019-10-01 16:36:30 +0200

    Aplicació Jocs

---
+++ Exercici Branques/Menu/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("LListat de Jocs");
+            Console.WriteLine();
+            Console.WriteLine("Tetris           [1]");
+            Console.WriteLine("BuscaMinas       [2]");
+            Console.WriteLine("LoL              [3]");
+            Console.WriteLine("Nostale          [4]");
+            Console.WriteLine("Metin2           [5]");
+            Console.WriteLine();
+            Console.WriteLine("Tria una opció --> ");
+
+            int opcio = Convert.ToInt32(Console.ReadLine());
+
+            if (opcio > 0 && opcio < 5)
+            {
+                switch(opcio)
+                {
+                    case 1:
+                        Tetris();
+                        break;
+                    case 2:
+                        Buscaminas();
+                        break;
+                    case 3:
+                        LoL();
+                        break;
+                    case 4:
+                        Nostale();
+                        break;
+                    case 5:
+                        Metin2();
+                        break;
+
+                }
+            }
+        }
+
+        public static void Tetris()
+        {
+            Console.WriteLine("Aixo es el Tetris");
+        }
+
+        public static void Buscaminas()
+        {
+            Console.WriteLine("Aixo es el Buscaminas");
+        }
+
+        public static void LoL()
+        {
+            Console.WriteLine("Aixo es el LoL");
+        }
+
+        public static void Nostale()
+        {
+            Console.WriteLine("Aixo es el Nostale");
+        }
+
+        public static void Metin2()
+        {
+            Console.WriteLine("Aixo es el Metin2");
+        }
+
+
+    }
+}
